2f31d0122b5ca822cd28d64240a322d097bac5ab
EmdadErgo-Ventures <emdad@ergo-ventures.com>
2022-03-10 13:17:52 +0600

    remaining preview

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.testcompose
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +21,23 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+
+
+    @Composable
+    fun PhotographerCard() {
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PhotographerCardPreview(){
+        }
+
 }
 
