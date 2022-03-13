e085cb2b2444d678d06bc688481a0281c88a76a7
EmdadErgo-Ventures <emdad@ergo-ventures.com>
2022-03-13 12:55:44 +0600

    created a BodyContent

--- app/src/main/java/com/example/testcompose/compose/scaffold/BodyContentComposable.kt
+++ app/src/main/java/com/example/testcompose/compose/scaffold/BodyContentComposable.kt
@@ -2,19 +2,22 @@ package com.example.testcompose.compose.scaffold
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.codelab.layouts.ui.TestComposeTheme
 
 @Composable
-fun ScaffoldCodeLab(
-
+fun BodyContentComposable(
+    modifier: Modifier
 ) {
-    Scaffold { innerPadding ->
-        BodyContentComposable(modifier = Modifier.padding(innerPadding))
+    Column(modifier = modifier) {
+        Text(
+            text = "Hi There! From Scaffold",
+        )
+
+        Text(
+            text = "Thanks for going through the layouts",
+        )
 
     }
 }
