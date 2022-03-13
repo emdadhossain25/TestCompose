e656492dbf94233824a3b13c4c775909eb06304b
EmdadErgo-Ventures <emdad@ergo-ventures.com>
2022-03-13 12:38:18 +0600

    fixed MainActivity

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.codelab.layouts.ui.TestComposeTheme
