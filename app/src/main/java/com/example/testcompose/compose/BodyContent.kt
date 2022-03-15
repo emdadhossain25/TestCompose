14f1148dc36d6f0180641737dc55b45d46f608a4
EmdadErgo-Ventures <emdad@ergo-ventures.com>
2022-03-15 14:47:12 +0600

    changed color

--- app/src/main/java/com/example/testcompose/compose/BodyContent.kt
+++ app/src/main/java/com/example/testcompose/compose/BodyContent.kt
@@ -4,16 +4,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun BodyContent(
     modifier: Modifier
 ) {
-    MyOwnColumn(modifier = modifier.padding(8.dp)) {
-        Text(text = "MyOwnColumn")
-        Text(text = "places items")
-        Text(text = "vertically")
-        Text(text = "We have done it by hand!")
+    MyOwnColumn(
+        modifier = modifier.padding(8.dp),
+    ) {
+        Text(
+            text = "MyOwnColumn",
+            color = Color.White
+        )
+        Text(
+            text = "places items",
+            color = Color.Yellow
+        )
+        Text(
+            text = "vertically",
+            color = Color.Green
+        )
+        Text(
+            text = "We have done it by hand!",
+            color = Color.Cyan
+        )
     }
 }
