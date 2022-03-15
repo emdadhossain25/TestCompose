a3c6d85f78a6fb8fbe003fcf59e0261cd3b4bded
EmdadErgo-Ventures <emdad@ergo-ventures.com>
2022-03-15 18:57:10 +0600

    finished layouts basics

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -10,13 +11,14 @@ import com.codelab.layouts.ui.TestComposeTheme
 import com.example.testcompose.compose.BodyContent
 import com.example.testcompose.compose.DecoupledConstraintLayout
 import com.example.testcompose.compose.StaggeredGrid
+import com.example.testcompose.compose.TwoTexts
 import com.example.testcompose.compose.scaffold.SimpleList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DecoupledConstraintLayout()
+
         }
     }
 
@@ -24,7 +26,10 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun ScaffoldCodeLabPreview() {
         TestComposeTheme {
-            DecoupledConstraintLayout()
+            Surface{
+
+                TwoTexts(text1 = "text1", text2 = "text2")
+            }
         }
     }
 }
