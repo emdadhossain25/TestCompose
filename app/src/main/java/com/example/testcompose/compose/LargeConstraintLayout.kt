f8c23e9a66c92f4169b2381e5b38d3d5b0726566
EmdadErgo-Ventures <emdad@ergo-ventures.com>
2022-03-15 18:20:23 +0600

    fixed LargeConstraintLayout.kt

--- app/src/main/java/com/example/testcompose/compose/LargeConstraintLayout.kt
+++ app/src/main/java/com/example/testcompose/compose/LargeConstraintLayout.kt
@@ -4,8 +4,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
 
 @Composable
 fun LargeConstraintLayout() {
@@ -17,7 +19,7 @@ fun LargeConstraintLayout() {
             "This is a very very very very very very very long text",
             Modifier.constrainAs(text) {
                 linkTo(start = guideline, end = parent.end)
-                width = Dimension.preferredWrapContent
+                width = Dimension.preferredWrapContent.atLeast(100.dp)
             }
 
         )
